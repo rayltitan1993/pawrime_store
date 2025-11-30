c96d0f04f40d4cb898e9ae4c1b7e5c1548e0478b
RayLLLLLL <44373899+rayltitan1993@users.noreply.github.com>
2025-11-30 20:28:15 +0800

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -3,7 +3,7 @@
 import { signIn } from "../../auth";
 import { AuthError } from "next-auth";
 
-export async function authenticate(prevState: string | undefined, formData: FormData) {
+export async function authenticate(prevState: { message: string } | undefined, formData: FormData) {
   try {
     await signIn("credentials", formData);
   } catch (error) {
